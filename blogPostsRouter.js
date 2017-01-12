ef49a98c1028f754ac721cbd94d420e43f2e23b5
Aleah Manzi <aleah.manzi@gmail.com>
2017-01-12 14:13:47 -0500

    fixing server/router files

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -85,3 +85,5 @@ app.delete('/blog-posts/:id', (req, res) => {
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+module.exports = router;
